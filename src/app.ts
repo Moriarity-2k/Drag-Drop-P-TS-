d43b8b86acb0efd270bb3f8a7737fd6b4d42036e
Moriarity-2k <professormoriarity2000@gmail.com>
2024-02-06 07:57:17 +0530

    Validator Fix - Success

--- src/app.ts
+++ src/app.ts
@@ -13,8 +13,84 @@ function AutoBind(target: any, name: string, propetyDesc: PropertyDescriptor) {
 	return adjusted;
 }
 
-function validatorTrim(target: any, name: string) {
-    console.log(target);
+interface validationObj {
+	value: string | number;
+	required?: boolean;
+	min?: number;
+	max?: number;
+	minLength?: number;
+	maxLength?: number;
+}
+
+function validatorTrim(valInput: validationObj) {
+	let valid = true;
+
+	if (valInput.required && !valInput.value.toString().trim()) valid = false;
+
+	if (
+		valInput.min &&
+		typeof valInput.value === "number" &&
+		valInput.value < valInput.min
+	)
+		valid = false;
+
+	if (
+		valInput.max &&
+		typeof valInput.value === "number" &&
+		valInput.value < valInput.max
+	)
+		valid = false;
+
+	if (
+		valInput.minLength &&
+		typeof valInput.value === "string" &&
+		valInput.value.toString().trim().length < valInput?.minLength
+	)
+		valid = false;
+
+	if (
+		valInput.maxLength &&
+		typeof valInput.value === "string" &&
+		valInput.value.length < valInput?.maxLength
+	)
+		valid = false;
+
+	return valid;
+}
+
+// To take the project list template from html and render
+
+class ProjectListRender {
+	templateEl: HTMLTemplateElement;
+	hostEl: HTMLDivElement;
+	element: HTMLElement;
+
+	constructor(private type: "active" | "finished") {
+		this.templateEl = document.getElementById(
+			"project-list"
+		) as HTMLTemplateElement;
+		this.hostEl = document.getElementById("app")! as HTMLDivElement;
+
+		const importedNode = document.importNode(this.templateEl, true);
+		this.element = importedNode.firstElementChild as HTMLElement;
+
+		this.element.id = `${this.type}-projects`;
+
+		this.attachTo_Host();
+		this.renderContent();
+	}
+
+	private renderContent() {
+		const listId = `${this.type}-projects-list`;
+
+		this.element.querySelector("ul")!.id = listId;
+		this.element.querySelector("h2")!.textContent =
+			this.type.toUpperCase() + " PROJECTS";
+	}
+
+	private attachTo_Host() {
+		this.hostEl.insertAdjacentElement("beforeend", this.element);
+	}
 }
 
 // To initiate from the template , we have in Html , (( WORKED FINE , glitch with binder))
@@ -53,12 +129,39 @@ class InputHolder {
 	}
 
 	private takingInput(): [string, string, number] | void {
-        
 		const title = this.titleInputElement.value;
 		const desc = this.descInputElement.value;
-		const no_people = +this.peopleInputElement.value;
+		const no_people = this.peopleInputElement.value;
 
-		return [title, desc, no_people];
+		const titleValid: validationObj = {
+			value: title,
+			required: true,
+		};
+
+		const descValid: validationObj = {
+			value: desc,
+			required: true,
+			minLength: 5,
+		};
+
+		const no_people_valid: validationObj = {
+			value: +no_people,
+			required: true,
+			min: 1,
+			max: 5,
+		};
+
+		if (
+			!(
+				validatorTrim(titleValid) &&
+				validatorTrim(descValid) &&
+				validatorTrim(no_people_valid)
+			)
+		) {
+			alert("Not a valid input , please try again !!!");
+			return;
+		}
+		return [title, desc, +no_people];
 	}
 
 	private clearInput() {
@@ -89,3 +192,5 @@ class InputHolder {
 }
 
 new InputHolder();
+new ProjectListRender("active");
+new ProjectListRender("finished");
